0c1c846245e212d540cd9fe4a4e5d9df2bd8c113
Derek Wheeler <frozenarrow73@gmail.com>
2023-03-24 13:17:17 -0600

    part 3 complete

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,11 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test("tests that shuffleArray returns an result of equal length", () => {
+        expect(shuffleArray([1, 2, 3, 4, 5])).toHaveLength(5)
+    })
+    test("tests that shuffleArray returns an array", () => {
+        expect(shuffleArray([1, 2, 3, 4, 5])).toBeInstanceOf(Array)
+    })
+
 })
